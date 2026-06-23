46ca4fc5f95a59e7ae62874efceaba11fbcc3c25
zuzannaff <zuzanna.fabianska@gmail.com>
2026-06-23 13:35:05 +0000

    check

--- Altercheck.java
+++ Altercheck.java
@@ -9,11 +9,18 @@ String eingabe = JOptionPane.showInputDialog("Wie alt bist du?");
 int alter = Integer.parseInt(eingabe);
 
 
+if (alter < 13) {
+    JOptionPane.showMessageDialog(null, "Kind");
+
+}
+if (alter >= 13 && alter < 17) {
+    JOptionPane.showMessageDialog(null, "Jugendlicher");
+}
 if (alter >= 18) {
-    JOptionPane.showMessageDialog(null, "volljaehrig");
-} else {
-    JOptionPane.showMessageDialog(null, "minderjaehrig");
+    JOptionPane.showMessageDialog(null, "Erwachsener");
+}
 }
 
     }
-}
+
+
